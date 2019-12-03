e06c74e93b48d4245ae6947af665336bdba4f046
Paulo Henrique Filipini <phfilipini@stefanini.com>
2019-12-03 15:29:16 -0300

    ajustando espaçamento

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -11,28 +11,20 @@ export class Autenticacao {
     constructor(private router: Router){}
 
     public cadastrarUsuario(usuario: Usuario): Promise<any> {
-        console.log('chegamos', usuario)
-
-
        return firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
         .then((resposta:any) =>{
             // removendo senha do atributo senha do objeto do usuario
             delete usuario.senha
 
-
             // registrando dados complementares do usuario no path email na base64
             firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
             .set(usuario)
-
-          
         })
         .catch((error: Error)=> {
             console.log(error)
         })
     }
-
-    public autenticar(email:string, senha:string): void {
-         
+    public autenticar(email:string, senha:string): void { 
         firebase.auth().signInWithEmailAndPassword(email,senha)
         .then((resposta: any) => {
             firebase.auth().currentUser.getIdToken()
@@ -42,24 +34,17 @@ export class Autenticacao {
                 this.router.navigate(['/home'])
             })
         })
-
         .catch((error: Error) => console.log(error))
-
     }
-    
     public autenticado(): boolean {
-
         if(this.token_id === undefined && localStorage.getItem('idToken') != null){
             this.token_id = localStorage.getItem('idToken')
         }
-
         if(this.token_id === undefined){
             this.router.navigate(['/'])
         }
-
         return this.token_id !== undefined
     }
-    
     public sair(): void{
         firebase.auth().signOut()
             .then(() => {
@@ -67,6 +52,5 @@ export class Autenticacao {
                 this.token_id = undefined
                 this.router.navigate(['/'])
             })
-        
     }
 }
