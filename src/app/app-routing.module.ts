03be6f0046952906187909d85e3532e9ec7cd3c1
Paulo Henrique Filipini <phfilipini@stefanini.com>
2019-12-06 15:49:33 -0300

    guard funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
 
 const routes: Routes = [
   {
@@ -9,11 +11,13 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+   
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'cadastro',
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Autenticacao } from './../autenticacao.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+ providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private autenticacao:Autenticacao){}
+
+ canActivate(
+   next: ActivatedRouteSnapshot,
+   state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+   let userAuthenticated ; // Get the current authentication state from a Service!
+
+   if (localStorage.getItem('idToken') != null) {
+     return false;
+   } else {
+     return true;
+   }
+ }
+}
