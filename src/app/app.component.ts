9b848e1126bf869680b07bc852f44add8e90f2d8
Paulo Henrique Filipini <phfilipini@stefanini.com>
2019-12-06 12:54:13 -0300

    menu aparecendo somente quando logado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ import * as firebase from 'firebase'
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+  providers: [Autenticacao]
 })
 export class AppComponent {
   public appPages = [
